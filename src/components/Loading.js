6aeaba83530af4130bfb5d739947ffa38d2da4fe
Johan Ljung <jollelj@hotmail.com>
2015-05-17 19:15:29 +0200

    Loader component now uses a loading gif instead of text

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,47 +2,16 @@
 
 var React = require('react/addons');
 
-
 require('styles/Loading.scss');
-
-var interval;
 
 var Loading = React.createClass({
 
-	getInitialState: function() {
-		return {
-			text: 'Loading',
-			loadingDots: ''
-		};
-	},
-
-	componentDidMount: function() {
-		interval = setInterval(this.updateText, 500);
-	},
-
-	componentWillUnmount: function() {
-		clearInterval(interval);
-	},
-
-	updateText: function() {
-		var currentText = this.state.text;
-		if ( this.state.loadingDots.length < 3 ) {
-			this.setState({
-				loadingDots: this.state.loadingDots + '.'
-			});
-		} else {
-			this.setState({
-				loadingDots: ''
-			});
-		}
-	},
-
   render: function () {
     return (
-        <span>{this.state.text + this.state.loadingDots}</span>
+        <img src="../images/loader.gif" />
       );
   }
 });
 
-module.exports = Loading; 
+module.exports = Loading;
 
